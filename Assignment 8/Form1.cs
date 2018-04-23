dd0c34c6b675d5166265d895ec6cd98c096d22b6
evetn7 <35403848+evetn7@users.noreply.github.com>
2018-04-23 11:25:06 -0400

    Add files via upload

---
+++ Assignment 8/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_8
+{
+    public partial class OnCampusTickets : Form
+    {
+        //string userInput;
+        
+
+        public OnCampusTickets()
+        {
+            InitializeComponent();
+            
+        }
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void musicalButton_Click(object sender, EventArgs e)
+        {
+
+
+            Musicals mName = new Musicals();
+            CostLabel.Text = mName.Name;
+            txtChosenEvent.Text = mName.MusicalName;
+            
+        }
+        
+
+        private void txtcustomerName_TextChanged(object sender, EventArgs e)
+        {
+
+           
+
+        }
+
+        private void txtChosenEvent_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void basketButton_Click(object sender, EventArgs e)
+        {
+           
+                txtChosenEvent.Text = String.Format("Basketball").ToString();
+
+            
+
+        }
+
+        private void playbutton_Click(object sender, EventArgs e)
+        {
+            
+
+                txtChosenEvent.Text = String.Format("Romeo & Juliet Play").ToString();
+
+            
+        }
+
+        private void CostLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ Assignment 8/Ticket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment_8
+{
+    public abstract class Ticket
+    {
+
+        private int quantity;
+        private double cost;
+        //private DateTime date;
+        //private DateTime time;
+
+        public Ticket()
+        {
+            //Defualt Constructor
+        }
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { this.quantity = quantity; }
+        }
+        public double Cost
+        {
+            get { return cost; }
+            set { this.cost = cost; }
+        }
+
+        public Ticket(int quantity, double cost)
+        {
+            this.quantity = quantity;
+            this.cost = cost;
+
+        }
+
+    }
+}
